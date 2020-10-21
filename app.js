b0012c5b077f41cc9c4655f14f21d30a0a8810e6
obasemola <obasemolaayodele@gmail.com>
2020-10-21 21:29:27 +0300

    using the graphql playground

--- app.js
+++ app.js
@@ -23,4 +23,37 @@ let persons = [
   }
 ]
 
-const 
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person]!
+    findPerson(name: String): Person
+  }
+`
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) =>
+      persons.find(p => p.name === args.name)
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+}) 
