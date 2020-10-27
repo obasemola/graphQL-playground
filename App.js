ed8b932a297e61302ca8b388cdff7622bcc85a44
obasemola <obasemolaayodele@gmail.com>
2020-10-27 18:40:19 +0200

    adding books and authors

--- App.js
+++ App.js
@@ -49,57 +49,6 @@ let authors = [
  * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
 */
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
 
 const typeDefs = gql`
 
@@ -155,13 +104,6 @@ const resolvers = {
         })
       })
 
-      // let returnedBooks = books.map((book) => {
-      //   if(args.author === book.author){
-      //     return book
-      //   }else if(book.genres.includes(args.genre)){
-      //     return book
-      //   }
-      // })
       returnedBooks = returnedBooks.filter(authorBook => authorBook !== undefined)
       return returnedBooks.populate('author', { name: 1, born: 1 })
 
@@ -188,32 +130,25 @@ const resolvers = {
     }
   },
 
-  // Book: {
-  //   author: (root) => {
-  //     const authorObject = Author.findOne({ name: root.author })
-  //     return {
-  //       author: root.author.populate({ name:  })
-  //     }
-  //   }
-  // },
-
   Mutation: {
     addBook: async (root, args) => {
       const authorObject = await Author.findOne({ name: args.author })
+      if(!authorObject){
+        const newAuthor = new Author({ name: args.author })
+        await newAuthor.save()
+
+        const savedAuthorObject = await Author.findOne({ name: args.author })
+        const book = new Book({
+          ...args,
+          author: savedAuthorObject })
+          return await book.save()
+      }
+
       const book = new Book({
         ...args,
         author: authorObject })
-        
-        console.log(authorObject._id)
-      const author = Author.findOne({ name: book.author })
-      if(author){
-        // console.log(authors)
-      } else {
-        const newAuthor = new Author({ name: book.author })
-        newAuthor.save()
-
-      }
-      return book.save()
+      
+      return await book.save()
     },
 
     editAuthor: (root, args) => {
